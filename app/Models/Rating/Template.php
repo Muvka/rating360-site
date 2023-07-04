aec61b8990b0ea471310021f51b4f55b31a3646f
Muvka <muvl1989@gmail.com>
2023-07-04 17:37:25 +0300

    Удалил soft deletes

--- app/Models/Rating/Template.php
+++ app/Models/Rating/Template.php
@@ -3,15 +3,10 @@
 namespace App\Models\Rating;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Database\Eloquent\Relations\HasManyThrough;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Template extends Model
 {
-    use SoftDeletes;
-
     protected $table = 'rating_templates';
 
     protected $guarded = [];
