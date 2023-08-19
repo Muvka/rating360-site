942345e54049827b81b085b79a36aa669dd89fc5
Muvka <muvl1989@gmail.com>
2023-08-19 08:21:29 +0300

    Сделал более гибким

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,41 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import fs from 'fs';
 import createSvgSpritePlugin from 'vite-plugin-svg-sprite';
 
-const host = 'rating360.loc';
+export default ({ mode }) => {
+	process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+	const host = process.env.VITE_DEV_HOST;
+	const certificatePath = process.env.VITE_DEV_CERTIFICATE_PATH;
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.scss', 'resources/js/app.jsx'],
-            refresh: true,
-        }),
-        createSvgSpritePlugin({
-            include: '**/icon-*.svg',
-            symbolId: '[name]-[hash]',
-        }),
-    ],
-    server: {
-        host,
-        hmr: { host },
-        https: {
-            key: fs.readFileSync(`/Applications/MAMP/Library/OpenSSL/certs/${host}.key`),
-            cert: fs.readFileSync(`/Applications/MAMP/Library/OpenSSL/certs/${host}.crt`),
-        },
-    },
-});
+	const config = defineConfig({
+		plugins: [
+			laravel({
+				input: ['resources/css/app.scss', 'resources/js/app.jsx'],
+				refresh: true
+			}),
+			createSvgSpritePlugin({
+				include: '**/icon-*.svg',
+				symbolId: '[name]-[hash]'
+			})
+		],
+		resolve: {
+			alias: {
+				'@': '/resources'
+			}
+		}
+	});
+
+	if (host && certificatePath) {
+		config.server = {
+			host,
+			hmr: { host },
+			https: {
+				key: fs.readFileSync(`${certificatePath}/${host}.key`),
+				cert: fs.readFileSync(`${certificatePath}/${host}.crt`)
+			}
+		};
+	}
+
+	return config;
+};
