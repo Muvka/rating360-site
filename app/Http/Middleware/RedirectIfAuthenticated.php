bd54ae4bc9ec049273eb26052166ed97547ed20c
Muvka <muvl1989@gmail.com>
2024-02-19 13:23:00 +0300

    Редирект на учебный портал для гостей

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use App\Providers\RouteServiceProvider;
+use App\Settings\AppGeneralSettings;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -18,11 +19,16 @@ class RedirectIfAuthenticated
     public function handle(Request $request, Closure $next, string ...$guards): Response
     {
         $guards = empty($guards) ? [null] : $guards;
+        $appGeneralSettings = app(AppGeneralSettings::class);
 
         foreach ($guards as $guard) {
             if (Auth::guard($guard)->check()) {
                 return redirect(RouteServiceProvider::HOME);
             }
+        }
+
+        if ($appGeneralSettings->moodle_auth_enabled && $appGeneralSettings->moodle_account_url) {
+            return redirect()->away($appGeneralSettings->moodle_account_url);
         }
 
         return $next($request);
